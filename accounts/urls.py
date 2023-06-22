35b56fdcf3fe4638f03652932a20a8f8df92dcec
tonydasr <136280620+tonydasr@users.noreply.github.com>
2023-06-22 01:02:42 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,7 @@
 from django.urls import include, re_path
 
 
-from .views import (
-    login_view,
-    register_view,
-    logout_view,
-    select_user,
-    change_password_view
-)
+from .views import *
 
 app_name = 'accounts'
 
@@ -16,6 +10,7 @@ urlpatterns = [
     re_path(r'^register/$', register_view, name='register'),
     re_path(r'^logout/$', logout_view, name='logout'),
     re_path(r'^select_user/$', select_user, name='select_user'),
+    re_path(r'^airline/$', airline, name='airline'),
     re_path(r'^change-password/$', change_password_view, name='change_password'),
 
 ]
