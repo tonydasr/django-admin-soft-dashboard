98b6c808aae37e027a22f2463afc9db2e7373cef
tonydasr <136280620+tonydasr@users.noreply.github.com>
2023-06-23 22:01:56 +0100

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import User, AccountDetails, UserAddress, Userpassword
+from bankingsystem.admin_actions import export_as_csv
 
 
 
@@ -51,8 +52,11 @@ class UserAddressAdmin(admin.ModelAdmin):
     # override the formfield_for_foreignkey method to show the full country name in the dropdown
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "country":
+
             kwargs["choices"] = UserAddressForm.COUNTRY_CHOICES
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(UserAddress, UserAddressAdmin)
+
+admin.site.add_action(export_as_csv, name='export_selected')
